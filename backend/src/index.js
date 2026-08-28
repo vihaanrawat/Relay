881e0c05ce7bd9c370b558c66b308639755cfa54
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-28 16:49:32 +0000

    Aug 28, 2026, 10:19 PM

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,6 @@ import "dotenv/config"
 
 const app = express()
 
-console.log("DB_URL=",process.env.DB_URL)
+console.log("DB_URL=",process.env.DB_URL);
 
 app.listen(3000, () => console.log("Server is up and running on port 3000"));
