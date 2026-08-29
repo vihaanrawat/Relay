2261b29d04324a724d5ecdd53187798194178b5d
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-29 05:55:23 +0000

    Aug 29, 2026, 11:25 AM

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,13 @@
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
-    
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
 })
