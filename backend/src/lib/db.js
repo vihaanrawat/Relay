eafb965736a07203abfc20def83771e76f7a5da1
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-29 13:48:14 +0000

    Aug 29, 2026, 7:18 PM

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,19 +1,18 @@
 import mongoose from "mongoose"
 
-async function connectDb() {
+export async function connectDb() {
     try {
         const mongoUri = process.env.MONGODB_URI
 
         if(!mongoUri){
             throw new Error("MONGODB_URI is required")
-            
-
+        
         }
         const conn = await mongoose.connect(mongoUri)
         console.log("MongoDB connected",conn.connection.host)
 
     } catch (error) {
-        console.log("MongoDB connection error:", error.message);
+        console.error("MongoDB connection error:",error.message);
         process.exit(1)
         //1 means failed , 0 means success
 
