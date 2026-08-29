4be2582c2ba83ec76312fa4020d6d054d868a25e
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-29 13:28:45 +0000

    Aug 29, 2026, 6:58 PM

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,7 +1,18 @@
 import mongoose from "mongoose"
 
 const messageSchema = new mongoose.Schema({
-    
+    senderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    recieverId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+
 },{timestamps:true})
 
 const Message = mongoose.model("Message",messageSchema)
