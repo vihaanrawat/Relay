45f32e78fd73831c3f60f32fa8e9d9d49aa22e2b
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-29 15:21:28 +0000

    Aug 29, 2026, 8:51 PM

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,6 @@ import "dotenv/config"
 import User from "./models/user.model.js"
 import { connectDB } from "./lib/db.js"
 
-
 const app = express()
 const PORT = process.env.PORT
 
@@ -15,3 +14,4 @@ app.listen(PORT, () => {
     connectDB();
     console.log("Server is up and running on PORT :  ", PORT)
 });
+
