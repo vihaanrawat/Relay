b388bfdb96850f3f9d8489e9f641379ddf37525b
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-29 17:02:29 +0000

    Aug 29, 2026, 10:32 PM

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,15 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import express from "express"
 import "dotenv/config"
 
-import 
+import { clerkMiddleware } from '@clerk/express'
 
 import User from "./models/user.model.js"
 import { connectDB } from "./lib/db.js"
 
 const app = express()
 const PORT = process.env.PORT
+
+app.use(clerkMiddleware())
 
 app.get("/health",(req,res) =>{
     res.status(200).json({ok:true});
