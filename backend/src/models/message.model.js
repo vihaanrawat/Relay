d379abc5598406e14168dbf9b99349ca73dea7c3
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-29 13:23:06 +0000

    Aug 29, 2026, 6:53 PM

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -1,1 +1,7 @@
 import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema({},{timestamps:true})
+
+const Message = mongoose.model("Message",messageSchema)
+
+export default Message
