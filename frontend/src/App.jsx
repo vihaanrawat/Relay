4bc616aef3f47617e6cfadab058ac0d9b69efbe8
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-30 03:46:15 +0000

    Aug 30, 2026, 9:16 AM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
 
 function App() {
 
   return (
     <div>
       <h1>MY APP</h1>
+       <header>
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
     </div>
   )
 }
