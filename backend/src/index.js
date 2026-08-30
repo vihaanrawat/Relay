adb5176872e9cf16bfdbe821ba7c2bb61a1e1a25
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-30 08:40:48 +0000

    Aug 30, 2026, 2:10 PM

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,12 @@ app.get("/health",(req,res) =>{
 
 if(fs.existsSync(publicDir)){
     app.use(express.static(publicDir))
+
+    app.get("/{*any}",(req,res,next) =>{
+        res.sendFile(path.join(publicDir,"index.html"),(err) => next(err));
+    });
 }
+
 
 app.listen(PORT, () => {
     connectDB();
