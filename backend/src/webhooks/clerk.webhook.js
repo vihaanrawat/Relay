d937c561388c26835fd67523bd7fcbf961f9d390
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-31 15:13:04 +0000

    Aug 31, 2026, 8:43 PM

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -5,7 +5,11 @@ import {verifyWebhook} from "@clerk/backend/webhooks"
 const router = express.Router()
 
 router.post("/",async(req,res)=>{
-    
+    const signingSecret = process.env.CLERK_WEBHOOK_SIGNING_SECRET
+    if(!signingSecret){
+        req.status(503).json({message:"Webhook secret is not provided"})
+        return;
+    }
 })
 
 export default router
