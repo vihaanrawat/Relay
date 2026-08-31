73ed7dd7360281e917212048596ff6ee508563f1
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-31 14:42:16 +0000

    Aug 31, 2026, 8:12 PM

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ const FRONTEND_URL= process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(),"public");
 
-app.use("/api/webhooks/clerk")
+app.use("/api/webhooks/clerk",express.raw({type:"application/json"}),clerkWebhook)
 
 app.use(express.json())
 app.use(cors({origin:FRONTEND_URL ,Credentials:true}))
