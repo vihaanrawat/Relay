184d87e5e46a9b5a84427e0c2df7c687dad011e4
Vihaan <vihaanrawat2007vr@gmail.com>
2026-08-31 15:09:29 +0000

    Aug 31, 2026, 8:39 PM

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,7 +1,11 @@
-import express from "express"
+import express, { response } from "express"
 import User from "../models/user.model.js"
 import {verifyWebhook} from "@clerk/backend/webhooks"
 
 const router = express.Router()
+
+router.post("/",async(req,res)=>{
+    
+})
 
 export default router
