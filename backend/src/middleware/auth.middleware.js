8f830407f5778e068e90b45968515c4045bc194f
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-02 17:49:43 +0000

    Sep 2, 2026, 11:19 PM

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -15,8 +15,12 @@ export async function protectRoute(req, res, next) {
             return;
         }
 
-        
+        req.user = user
+        next()
     } catch (error) {
+
+        console.error("Error in protectRoute middleware:",error.message)
+        res.status(500).json({message:"Internal Server Error"});
         
     }
 }
