7d9923f5e02f01e76e7e8b32461de679e34dc7c5
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-02 17:39:41 +0000

    Sep 2, 2026, 11:09 PM

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,5 +2,21 @@ import { getAuth } from "@clerk/express";
 import User from "../models/user.model";
 
 export async function protectRoute(req, res, next) {
+    try {
+        const {userId} = getAuth(req)
+        if(!userId){
+            res.status(401).json({message:"Unauthorized"});
+            return;
+        }
+        const user = await User.findOne({clerkId:userId})
 
+        if(!userId){
+            res.status(401).json({message:"User profile is not synced yet"})
+            return;
+        }
+
+        
+    } catch (error) {
+        
+    }
 }
