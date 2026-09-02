067c1c643ef737d5199e50734b57228dc6c2c7ec
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-02 23:24:14 +0530

    implement authentication routes and middleware for user verification

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -2,5 +2,6 @@ export async function checkAuth(req, res, next) {
     if(!req.user){
         return res.status(401).json({message:"User unauthorized"});
     }
+
     res.status(200).json(req.user)
 }
