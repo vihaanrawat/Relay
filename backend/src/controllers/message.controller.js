2d539a71ef647f69b612e69e5ad0327df940138a
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-04 05:28:10 +0000

    Sep 4, 2026, 10:58 AM

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,8 +1,12 @@
-export async function getUsersForSidebar(req,res) {
+import User from "../models/user.model.js"
+
+export async function getUsersForSidebar(req, res) {
     try {
+        const loggedInUserId = req.user._id
         
+        const filteredUsers = await User.find({_id:{$ne : loggedInUserId}})
     } catch (error) {
-        
+
     }
-    
+
 }
