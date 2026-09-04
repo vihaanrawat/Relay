097f17e45ca09c23958018b2d32aa31be4654a71
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-04 05:31:45 +0000

    Sep 4, 2026, 11:01 AM

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -4,8 +4,12 @@ export async function getUsersForSidebar(req, res) {
     try {
         const loggedInUserId = req.user._id
         
-        const filteredUsers = await User.find({_id:{$ne : loggedInUserId}})
+        const filteredUsers = await User.find({_id:{$ne : loggedInUserId}}).select("-clerkId")
+
+        res.status(200).json(filteredUsers)
     } catch (error) {
+        console.error("Error in getUsersForSidebar:", error.message)
+        res.status(500).json({message : "Internal server error"})
 
     }
 
