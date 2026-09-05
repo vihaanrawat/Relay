cb3ef0e281cd6fd76700ccb558bc39bb50c3f552
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-05 07:24:57 +0000

    Sep 5, 2026, 12:54 PM

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,16 +1,16 @@
 import express from "express";
-
 import {
     getConversationsForSidebar,
     getUsersForSidebar,
 } from "../controllers/message.controller.js"
-
 import { protectRoute } from "../middleware/auth.middleware.js";
+
 
 const router = express.Router()
 
 router.get("/users", protectRoute ,getUsersForSidebar)
-
 router.get("/conversations", protectRoute ,getConversationsForSidebar)
+router.get("/:id",protectRoute,getMessages)
+
 
 export default router
