20888aff55b57c1c82c14585bf56bab85dbb56b7
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-05 07:50:15 +0000

    Sep 5, 2026, 1:20 PM

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -73,3 +73,12 @@ export async function getMessages(req, res) {
 
     }
 }
+
+export async function sendMessage(req,res) {
+    try {
+        
+    } catch (error) {
+        
+    }
+    
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,19 +3,21 @@ import {
     getConversationsForSidebar,
     getUsersForSidebar,
     getMessages,
+    sendMessage,
 } from "../controllers/message.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { upload } from "../middleware/upload.middleware.js"
 
 
 const router = express.Router()
 
 router.use(protectRoute)
 
-router.get("/users",getUsersForSidebar)
-router.get("/conversations" ,getConversationsForSidebar)
-router.get("/:id",getMessages)
-router.post("/send/:id",upload.single("media"),sendMessage)
-
+router.get("/users", getUsersForSidebar)
+router.get("/conversations", getConversationsForSidebar)
+router.get("/:id", getMessages)
+router.post("/send/:id", upload.single("media"), sendMessage)
+//todo : show this in frontend
 
 
 export default router
