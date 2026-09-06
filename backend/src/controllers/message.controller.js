b61a5ba9f36e79448fb15dd56611b27025190785
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-06 07:15:36 +0000

    Sep 6, 2026, 12:45 PM

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js"
 import Message from "../models/message.model.js"
 import { uploadChatMedia } from "../lib/imagekit.js"
-import { getReceiverSocketId } from "../lib/socket.js"
+import { getReceiverSocketId, io } from "../lib/socket.js"
 
 export async function getUsersForSidebar(req, res) {
     try {
@@ -110,8 +110,10 @@ export async function sendMessage(req, res) {
         //realtime with socket.io
 
         const receiverSocketId = getReceiverSocketId(receiverId)
-
-        
+        //only send the message in realtime if user is online 
+        if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage", newMessage)
+        }
 
 
         res.status(201).json(newMessage)
