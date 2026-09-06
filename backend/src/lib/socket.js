9b8cc1d96097c4570b16e722434b7737f61c97d5
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-06 06:02:57 +0000

    Sep 6, 2026, 11:32 AM

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,4 +9,7 @@ const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173"
 
 const io = new Server(server , {cors : {origin: [allowedOrigin]}})
 
-io.on("connection")
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId
+
+})
