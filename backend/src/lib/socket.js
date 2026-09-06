0f556a427cf1e0a70eb6aab5211ce4c9352f8263
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-06 06:38:55 +0000

    Sep 6, 2026, 12:08 PM

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -17,4 +17,7 @@ const userSocketMap = {};
 io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId
     if(userId) userSocketMap[userId] = socket.id
+
+    
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
 })
