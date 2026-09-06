8b8920888e60a4a8100a44c45bd9c2aaade1d3b9
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-06 07:03:18 +0000

    Sep 6, 2026, 12:33 PM

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,6 +9,10 @@ const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:5173"
 
 const io = new Server(server , {cors : {origin: [allowedOrigin]}})
 
+function getReceiverSocketId(userId) {
+    return userSocketMap[userId];
+}
+
 
 //online users map = {userId: socketId}
 const userSocketMap = {};
@@ -29,3 +33,5 @@ io.on("connection", (socket) => { // connection -> special event
 
     })
 })
+
+export {app}
