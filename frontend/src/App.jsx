c55821bb25afa6e6346123fbecef88471cb7f114
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-10 04:52:36 +0000

    Sep 10, 2026, 10:22 AM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      <h1 className='text-4xl'>MY APP</h1>
+      <h1 className='text-4xl text-red-500 bg-blue-400'>MY APP</h1>
        <header>
         <Show when="signed-out">
           <SignInButton mode='modal'/>
