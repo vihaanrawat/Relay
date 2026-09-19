70b5528b8fb00fc6ea1a9fa998e2157c4e92e544
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-19 04:18:04 +0000

    Sep 19, 2026, 9:48 AM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 import { Button } from '@heroui/react';
 import {WallpaperProvider} from "./context/WallpaperContext"
+import {ThemeProvider} from "./context/ThemeContext"
 
 function App() {
 
