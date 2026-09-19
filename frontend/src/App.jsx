d7802a3ed0e3886a2d821f6e1122256db2a62ffd
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-19 04:18:58 +0000

    Sep 19, 2026, 9:48 AM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,19 +8,7 @@ function App() {
   return (
     <ThemeProvider>
     <WallpaperProvider>
-      <h1 className='text-4xl text-red-500 bg-blue-400'>MY APP</h1>
-      <Button>
-        My Button
-      </Button>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode='modal' />
-          <SignUpButton mode='modal' />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
+      
     </WallpaperProvider>
     </ThemeProvider>
   )
