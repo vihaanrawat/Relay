2a11b46c7c524e236a7dbedb90b6e1929a8bdb63
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-20 04:27:05 +0000

    Sep 20, 2026, 9:57 AM

---
+++ frontend/src/components/auth/AuthActionPanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthActionPanel() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthActionPanel
--- frontend/src/components/auth/AuthHeader.jsx
+++ frontend/src/components/auth/AuthHeader.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+
+function AuthHeader() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthHeader
 
