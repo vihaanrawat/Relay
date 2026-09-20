5a35e419c155b6b6c6070620445b344dec2d800a
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-20 04:09:35 +0000

    Sep 20, 2026, 9:39 AM

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,8 +1,11 @@
 function AuthPage() {
 
   return (
-    <div>
-      AuthPage
+    <div className="box-border flex min-h-dvh flex-col p-3 sm:p-5 md:p-8">
+      <div>
+        
+      </div>
+
     </div>
   )
 }
