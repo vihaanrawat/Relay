0c6f22ee38f5732a9662c082ad5ebc3ef1eee7da
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-20 04:14:12 +0000

    Sep 20, 2026, 9:44 AM

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -6,6 +6,12 @@ function AuthPage() {
       <div className="mx-auto flex w-full max-w-368 flex-1 flex-col overflow-hidden rounded-3xl border border-border bg-background text-foreground">
 
         <AuthHeader />
+        <main className="relative flex flex-1 flex-col overflow-hidden md:flex-row">
+
+          <AuthHeroPanel/>
+          <AuthActionPanel/>
+
+        </main>
 
 
       </div>
