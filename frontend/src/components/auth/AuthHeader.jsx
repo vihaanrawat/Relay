9204f96844731021c2fcb3ea78a8cceb11fe673d
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-21 08:46:30 +0000

    Sep 21, 2026, 2:16 PM

--- frontend/src/components/auth/AuthHeader.jsx
+++ frontend/src/components/auth/AuthHeader.jsx
@@ -1,5 +1,5 @@
 import { APP_NAME, AppLogo } from "../AppLogo"
-import {ThemePresetPicker} from "../ThemePresetPicker"
+import ThemePresetPicker from "../ThemePresetPicker"
 import {ThemeToggle} from "../ThemeToggle"
 import {WallpaperPicker} from "../WallpaperPicker"
 
