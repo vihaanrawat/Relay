1ee36c0246ba4104f6241d34f305bbedb6a5b221
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-21 08:37:13 +0000

    Sep 21, 2026, 2:07 PM

--- frontend/src/components/ThemePresetPicker.jsx
+++ frontend/src/components/ThemePresetPicker.jsx
@@ -1,7 +1,8 @@
+import React from "react"
+
 function ThemePresetPicker() {
   return (
-    <div>
-      
+    <div> 
     </div>
   )
 }
--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -1,7 +1,8 @@
+import React from "react"
+
 function ThemeToggle() {
   return (
     <div>
-      
     </div>
   )
 }
