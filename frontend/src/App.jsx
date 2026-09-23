dd2660f74f59c288bd84836ff872b6cae1df883e
Vihaan <vihaanrawat2007vr@gmail.com>
2026-09-23 13:49:43 +0000

    Sep 23, 2026, 7:19 PM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router';
 import ChatPage from './pages/ChatPage';
 import AuthPage from "./pages/AuthPage";
 import { useAuth } from '@clerk/react'
+import PageLoader from "./components/PageLoader";
 
 
 function App() {
